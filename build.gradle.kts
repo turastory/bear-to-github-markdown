d71b12e5367115789334962ee03ada2c0376d3e3
Yoon Ho Na <yh.na@riiid.co>
2020-03-02 10:21:42 +0900

    :tada: Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.3.61"
+}
+
+group = "com.turastory"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.3")
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:4.0.0-BETA1")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
