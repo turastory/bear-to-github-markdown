d3de6102348bfe47008fc6be1b6cb00b9c14809a
Yoon Ho Na <yh.na@riiid.co>
2020-03-02 14:44:35 +0900

    :sparkles: Add Converter

---
+++ src/main/kotlin/com/turastory/markdown/Converter.kt
@@ -0,0 +1,39 @@
+package com.turastory.markdown
+
+typealias LineConverter = (String) -> List<String>
+
+/**
+ * Converter reads lines of texts and generates another texts,
+ * depending on the rules provided.
+ */
+interface Converter {
+    operator fun invoke(lines: List<String>, rules: Map<Type, LineConverter>): List<String>
+}
+
+fun Converter(classifier: Classifier): Converter = object : Converter {
+    override fun invoke(lines: List<String>, rules: Map<Type, LineConverter>): List<String> {
+        val convertWithType: LineConverter = { line ->
+            val type = classifier(line)
+            rules[type]?.invoke(line) ?: listOf(line)
+        }
+
+        var skipConversion = false
+
+        // TODO: Refactor this - not to use state variable
+        return lines.flatMap { line ->
+            val type = classifier(line)
+            if (skipConversion) {
+                if (type == Type.Code) {
+                    skipConversion = false
+                    convertWithType(line)
+                } else {
+                    listOf(line)
+                }
+            } else {
+                if (type == Type.Code)
+                    skipConversion = true
+                convertWithType(line)
+            }
+        }
+    }
+}
---
+++ src/test/kotlin/com/turastory/markdown/ConverterTest.kt
@@ -0,0 +1,53 @@
+package com.turastory.markdown
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class ConverterTest : FunSpec({
+    val simpleClassifier = object : Classifier {
+        override fun invoke(line: String): Type {
+            return when {
+                line.startsWith("q") -> Type.Question
+                line.startsWith("a") -> Type.Answer
+                line.startsWith("c") -> Type.Code
+                else -> Type.None
+            }
+        }
+    }
+    val rules: Map<Type, LineConverter> = mapOf(
+        Type.Question to { line: String -> listOf("qq") },
+        Type.Answer to { line: String -> listOf("aa") },
+        Type.Code to { line: String -> listOf("```") },
+        Type.None to { line: String -> listOf("nn") }
+    )
+    val converter = Converter(simpleClassifier)
+
+    test("converter should skip converting the codes") {
+        val inputs = listOf(
+            "Hello World!",
+            "q: This is a question.",
+            "a: This is an answer.",
+            "c: Code start",
+            "q Android",
+            "a ABTest",
+            "c: Code end"
+        )
+        val outputs = listOf(
+            "nn",
+            "qq",
+            "aa",
+            "```",
+            "q Android",
+            "a ABTest",
+            "```"
+        )
+
+        converter(inputs, rules) shouldBe outputs
+    }
+
+    test("converter with a single line of text") {
+        val inputs = listOf("a: This is an answer")
+        val outputs = listOf("aa")
+        converter(inputs, rules) shouldBe outputs
+    }
+})
